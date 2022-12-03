ce5abf5a9027352a25d7098f8c482f7023c424c4
lukabrown <117334114+lukabrown@users.noreply.github.com>
2022-12-03 13:27:49 -0600

    touchups
    removed warnings, improved outputs, reordered code for improved reading

--- game.cpp
+++ game.cpp
@@ -1,7 +1,6 @@
 //Luka Brown Project 2
 
 #include <iostream>
-#include <cstdio>
 #include <cstdlib>
 #include <vector>
 #include <pthread.h>
@@ -16,8 +15,11 @@ static void* Player(void*);
 static void  PrintDeck();
 static void  PrintScreen();
 static void  ShuffleDeck();
+static char  AssignSuit(int card);
 
 static vector<int> deck;
+static pthread_mutex_t table;
+static ofstream MyFile("output.txt");
 static bool game = true;
 static int  wins = 0;
 static int  turns = 0;
@@ -26,21 +28,19 @@ static bool roundGoing = false;
 static int  prevWin = -1;
 static int  hand[] = { -1,-1,-1,-1,-1 };
 static int  draw[] = { -1,-1,-1,-1,-1 };
-static pthread_mutex_t table;
-static ofstream MyFile("output.txt");
 static bool alldone = false;
-static int seed = 0;
+static int  seed = 0;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "\nUSAGE: ./game.cpp [seed]\n");
         exit(1);
     }
+
     string temp = argv[1];
     for (int i = 0; temp[i] != '\0'; i++) {
         seed = seed * 10 + temp[i] - '0';
     }
-    //cout << "Seed: " << seed << '\n';
     srand((unsigned int)seed);
 
     pthread_t* const handle = new pthread_t [5];
@@ -48,9 +48,9 @@ int main(int argc, char *argv[]) {
 
     for (int thread = 0; thread < 5; thread++) {
         if (thread == 0)
-            pthread_create(&handle[thread], NULL, Dealer, (void*)thread);
+            pthread_create(&handle[thread], NULL, Dealer, (void*)(uintptr_t)thread);
         else
-            pthread_create(&handle[thread], NULL, Player, (void*)thread);
+            pthread_create(&handle[thread], NULL, Player, (void*)(uintptr_t)thread);
     }
 
     while (game) {
@@ -78,18 +78,10 @@ static void InitDeck() {
 static void PrintDeck() {
     cout << "Deck: ";
     char suit;
-    int temp;
+
     for(int i = 0; i < deck.size(); i++) {
-        temp = deck[i]/13;
-        if (temp == 0)
-            suit = 'C';
-        else if (temp == 1)
-            suit = 'D';
-        else if (temp == 2)
-            suit = 'H';
-        else 
-            suit = 'S';
-        cout << suit << deck[i]%13 << " ";
+        suit = AssignSuit(deck[i]);
+        cout << suit << deck[i]%13+1 << " ";
         if (((i+1) % 20) == 0) 
             cout << "\n      ";
     }
@@ -98,38 +90,23 @@ static void PrintDeck() {
 
 static void PrintScreen() {
     char suit;
-    int temp;
     string win;
+
     for (int i = 1; i < 5; i++) {
-        temp = hand[i]/13;
-        if (temp == 0)
-            suit = 'C';
-        else if (temp == 1)
-            suit = 'D';
-        else if (temp == 2)
-            suit = 'H';
-        else 
-            suit = 'S';
+        suit = AssignSuit(hand[i]);
 
         if (i % 2 == prevWin % 2)
             win = "Winner\n";
         else 
             win = "Lost\n";
+
         if (i == prevWin) {
-        cout << "Player " << i << ": hand " << suit << hand[i]%13 << ',';
-        temp = draw[i]/13;
-        if (temp == 0)
-            suit = 'C';
-        else if (temp == 1)
-            suit = 'D';
-        else if (temp == 2)
-            suit = 'H';
-        else 
-            suit = 'S';
-        cout << suit << draw[i]%13 << '\n';
+            cout << "Player " << i << ": hand " << suit << hand[i]%13+1 << ',';
+            suit = AssignSuit(draw[i]);
+            cout << suit << draw[i]%13+1 << '\n';
         }
         else
-            cout << "Player " << i << ": hand " << suit << hand[i]%13 << '\n';
+            cout << "Player " << i << ": hand " << suit << hand[i]%13+1 << '\n';
         cout << "Player " << i << ": " << win;
     }
     PrintDeck();
@@ -137,19 +114,22 @@ static void PrintScreen() {
 }
 
 static void* Dealer(void* id) {
-    int my_id = *((int*)(&id));
+    int my_id = *((int*)&id);
     srand((unsigned int)seed);
+
     while(game) {
         pthread_mutex_lock(&table);
         if (whos_turn == my_id) {
             InitDeck();
             ShuffleDeck();
+
             turns = 0;
+            roundGoing = true;
             if (wins == 0)
                 whos_turn = 1;
             else
                 whos_turn = 2;
-            roundGoing = true;
+            
             MyFile << "\nDealer: shuffle\n";
         }
         pthread_mutex_unlock(&table);
@@ -159,8 +139,10 @@ static void* Dealer(void* id) {
 
 static void* Player(void* id) {
     int my_id = *((int*)(&id));
-    long partner;
+    int partner;
+    char suit;
     srand((unsigned int)seed);
+
     if (my_id == 1)
         partner = 3;
     else if (my_id == 2) 
@@ -190,19 +172,24 @@ static void* Player(void* id) {
                 hand[my_id] = deck[0];
                 deck.erase(deck.begin());
                 whos_turn++;
+
                 if (whos_turn == 2 && wins == 1)
                     turns += 4;
+
                 if (wins == 1 && whos_turn == 5)
                     whos_turn = 1;
-                
+
                 if (whos_turn == 5) {
                     whos_turn -= 4;
                     turns += 4;
                 }
-                MyFile << "Player " << my_id << ": draw " << hand[my_id]%13 << '\n';
+                
+                suit = AssignSuit(hand[my_id]);
+                MyFile << "Player " << my_id << ": draw " << suit << hand[my_id]%13+1 << '\n';
             } else {
                 turns++;
                 whos_turn++;
+
                 if (whos_turn == 5) {
                     whos_turn -= 4;
                     turns += 4;
@@ -210,14 +197,21 @@ static void* Player(void* id) {
                 draw[my_id] = deck[0];
                 deck.erase(deck.begin());
                 
-                MyFile << "Player " << my_id << ": hand " << hand[my_id]%13 << '\n';
-                MyFile << "Player " << my_id << ": draw " << draw[my_id]%13 << '\n';
-                MyFile << "Player " << my_id << ": hand (" << hand[my_id]%13 << ',' << draw[my_id]%13;
-                MyFile << ") <> Player " << partner << ": hand " << hand[partner]%13 << "\n";
+                suit = AssignSuit(hand[my_id]);
+                MyFile << "Player " << my_id << ": hand " << suit << hand[my_id]%13+1 << '\n';
+                suit = AssignSuit(draw[my_id]);
+                MyFile << "Player " << my_id << ": draw " << suit << draw[my_id]%13+1 << '\n';
+                suit = AssignSuit(hand[my_id]);
+                MyFile << "Player " << my_id << ": hand (" << suit << hand[my_id]%13+1 << ',';
+                suit = AssignSuit(draw[my_id]);
+                MyFile << suit << draw[my_id]%13+1 << ") <> Player ";
+                suit = AssignSuit(hand[partner]);
+                MyFile << partner << ": hand " << suit << hand[partner]%13+1 << "\n";
 
                 if (hand[my_id]%13 == hand[partner]%13 || draw[my_id]%13 == hand[partner]%13) {
                     wins++;
                     roundGoing = false;
+
                     if (my_id % 2 == 0) 
                         MyFile << "Player " << my_id << ": Team 2 wins\n";
                     else
@@ -226,16 +220,20 @@ static void* Player(void* id) {
                     if (whos_turn == my_id) {
                         whos_turn = 2;
                     }
+
                     MyFile << "Player " << my_id << ": exits round\n";
                     prevWin = my_id;
                     PrintScreen();
                 } else {
                     int num = rand()%2;
                     if (num == 0) {
-                        MyFile << "Player " << my_id << ": discard " << hand[my_id]%13 << " at random\n";
+                        suit = AssignSuit(hand[my_id]);
+                        MyFile << "Player " << my_id << ": discard " << suit << hand[my_id]%13+1 << " at random\n";
                         hand[my_id] = draw[my_id];
-                    } else
-                        MyFile << "Player " << my_id << ": discard " << draw[my_id]%13 << " at random\n";
+                    } else {
+                        suit = AssignSuit(draw[my_id]);
+                        MyFile << "Player " << my_id << ": discard " << suit << draw[my_id]%13+1 << " at random\n";
+                    }
                     draw[my_id] = -1;
                 }
                 
@@ -254,4 +252,19 @@ void ShuffleDeck() {
         deck[i] = deck[random];
         deck[random] = swap;
     }
+}
+
+static char AssignSuit(int card) {
+    char suit;
+    int temp = card/13;
+
+    if (temp == 0)
+        suit = 'C';
+    else if (temp == 1)
+        suit = 'D';
+    else if (temp == 2)
+        suit = 'H';
+    else 
+        suit = 'S';
+    return suit;
 }
